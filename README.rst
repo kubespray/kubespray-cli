ea2932b518e6bce79152fbdc92c05fbd4c3bb7a1
Smana <smainklh@gmail.com>
2016-03-22 22:09:46 +0100

    Create a class for the deployment

--- README.rst
+++ README.rst
@@ -52,10 +52,59 @@ On **baremetal**
 
 ::
 
+    kubespray prepare -h
+    usage: kubespray prepare [-h] --nodes N [N ...] --masters N [N ...]
+                             [-p KUBESPRAY_PATH]
+    
+    optional arguments:
+      -h, --help            show this help message and exit
+      --nodes N [N ...]     List of nodes
+      --masters N [N ...]   List of masters
+      -p KUBESPRAY_PATH, --path KUBESPRAY_PATH
+                            Where the Ansible playbooks are installed
+
+::
+
     kubespray preprare --nodes node1[ansible_ssh_host=10.99.21.1] node2[ansible_ssh_host=10.99.21.2] node3[ansible_ssh_host=10.99.21.3] \
                        --masters node1 node2 \
 
-On cloud providers create vms and generate the inventory **AWS**
+On cloud providers create vms and generate the inventory
+
+**AWS**
+**warn** : not implemented yet
+
+::
+
+    usage: kubespray aws [-h] [--access-key AWS_ACCESS_KEY]
+                         [--secret-key AWS_SECRET_KEY] [--type AWS_INSTANCE_TYPE]
+                         [--keypair AWS_KEYPAIR_NAME] [--region AWS_REGION]
+                         [--security-group AWS_SECURITY_GROUP]
+                         [--vpc-id AWS_VPC_ID] [--vpc-subnet AWS_VPC_SUBNET]
+                         [--ami AWS_AMI] [--instances CLOUD_INSTANCES_COUNT]
+                         [--masters CLOUD_MASTERS_COUNT]
+    
+    optional arguments:
+      -h, --help            show this help message and exit
+      --access-key AWS_ACCESS_KEY
+                            AWS access key
+      --secret-key AWS_SECRET_KEY
+                            AWS secret key
+      --type AWS_INSTANCE_TYPE
+                            AWS instance type
+      --keypair AWS_KEYPAIR_NAME
+                            AWS key pair name
+      --region AWS_REGION   AWS region
+      --security-group AWS_SECURITY_GROUP
+                            AWS security group
+      --vpc-id AWS_VPC_ID   EC2 VPC id
+      --vpc-subnet AWS_VPC_SUBNET
+                            EC2 VPC regional subnet
+      --ami AWS_AMI         AWS AMI
+      --instances CLOUD_INSTANCES_COUNT
+                            Number of nodes
+      --masters CLOUD_MASTERS_COUNT
+                            Number of masters
+    
 
 ::
 
@@ -63,6 +112,9 @@ On cloud providers create vms and generate the inventory **AWS**
     [ --aws_access_key <access_key> --aws_secret_key <secret_key> ] [--coreos]
 
 **GCE**
+**warn** : not implemented yet
+
+example:
 
 ::
 
@@ -72,16 +124,31 @@ On cloud providers create vms and generate the inventory **AWS**
 Deploy cluster
 ~~~~~~~~~~~~~~
 
+example: Deploy a kubernetes cluster on CoreOS servers located on GCE
+
 ::
 
-    kubespray deploy \
-    [-n|--network-plugin <net_plugin>] \
-    [--aws|--gce] \
-    [--upgrade] \
-    [--ansible_opts] \
-    [-i|--inventory <inventory>] \
-    [--coreos] \
-    [-p|--path <kubespray_path>
+    kubespray deploy -n weave -p /tmp/kubespray --gce --coreos
+
+::
+
+    usage: kubespray deploy [-h] [-n {flannel,weave,calico}] [--aws] [--gce]
+                            [--upgrade] [--coreos] [--non-interactive]
+                            [-p KUBESPRAY_PATH] [--ansible_opts ANSIBLE_OPTS]
+    
+    optional arguments:
+      -h, --help            show this help message and exit
+      -n {flannel,weave,calico}, --network-plugin {flannel,weave,calico}
+      --aws                 Kubernetes deployment on AWS
+      --gce                 Kubernetes deployment on GCE
+      --upgrade             Upgrade Kubernetes cluster
+      --coreos              bootstrap python on CoreOS
+      --non-interactive     Don't prompt user for input
+      -p KUBESPRAY_PATH, --path KUBESPRAY_PATH
+                            Where the Ansible playbooks are installed
+      --ansible_opts ANSIBLE_OPTS
+                            Ansible options
+
 
 - default network plugin : flannel (vxlan) default
 - inventory path : "home/<current_user/kubespray/inventory.cfg".
@@ -92,6 +159,7 @@ Deploy cluster
 
 Infos
 ~~~~~
+**warn** : not implemented yet
 
 ::
 
---
+++ src/kubespray/deploy.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of Kubespray.
+#
+#    Foobar is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Foobar is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+kubespray.deploy
+~~~~~~~~~~~~
+
+Deploy a kubernetes cluster. Run the ansible-playbbook
+"""
+
+import sys
+import os
+import re
+import signal
+from subprocess import PIPE,STDOUT,Popen,check_output,CalledProcessError
+from kubespray.common import get_logger,query_yes_no
+from ansible.utils.display import Display
+display = Display()
+
+class RunPlaybook(object):
+    '''
+    Run the Ansible playbook to deploy the kubernetes cluster
+    '''
+    def __init__(self,options):
+        self.options = options
+        self.inventorycfg = os.path.join(options['kubespray_path'], 'inventory/inventory.cfg')
+        self.logger = get_logger(options.get('logfile'), options.get('loglevel'))
+
+    def ssh_prepare(self):
+        '''
+        Run ssh-agent and store identities
+        '''
+        try:
+            sshagent = check_output('ssh-agent')
+        except subprocess.CalledProcessError as e:
+            display.error('Cannot run the ssh-agent : %s' % e.output)
+        # Set environment variables
+        ssh_envars = re.findall('\w*=[\w*-\/.*]*', sshagent)
+        for v in ssh_envars:
+            os.environ[v.split('=')[0]] = v.split('=')[1]
+        # Store ssh identity
+        try:
+            proc = Popen('ssh-add', stdout=PIPE, stderr=STDOUT, stdin=PIPE, shell=True)
+            proc.stdin.write('password\n')
+            proc.stdin.flush()
+            response_stdout, response_stderr = proc.communicate()
+            display.display(response_stdout)
+        except CalledProcessError as e:
+            display.error('Failed to store ssh identity : %s' % e.output)
+        if response_stderr:
+            display.error(response_stderr)
+            self.logger.critical('Deployment stopped because of ssh credentials' % self.filename)
+            os.kill(int(os.environ.get('SSH_AGENT_PID')), signal.SIGTERM)
+            sys.exit(1)
+
+    def run_command(self, cmd):
+        '''
+        Execute a system command
+        '''
+        try:
+            proc = Popen(cmd, stdout=PIPE, stderr=STDOUT, universal_newlines=True, shell=False)
+            with proc.stdout:
+                for line in iter(proc.stdout.readline, b''):
+                     print(line),
+            proc.wait()
+        except CalledProcessError as e:
+            display.error('Deployment failed: %s' % e.output)
+            self.logger.critical('Deployment failed: %s' % e.output)
+            os.kill(int(os.environ.get('SSH_AGENT_PID')), signal.SIGTERM)
+            sys.exit(1)
+
+    def check_ping(self):
+        '''
+         Check if hosts are reachable
+        '''
+        display.banner('CHECKING SSH CONNECTIONS')
+        cmd = [os.path.join(self.options['ansible_path'], 'ansible'), '--ssh-extra-args', '-o StrictHostKeyChecking=no',
+               '-u',  '%s' % self.options['ansible_user'], '-e', 'ansible_ssh_user=%s' % self.options['ansible_user'],
+               '-b', '--become-user=root', '-m', 'ping','all',  '-i', self.inventorycfg]
+        self.run_command(cmd)
+        display.display('All hosts are reachable', color='green')
+
+    def deploy_kubernetes(self):
+        '''
+        Run the ansible playbook command
+        '''
+        cmd = [os.path.join(self.options['ansible_path'], 'ansible-playbook'), '--ssh-extra-args', '-o StrictHostKeyChecking=no',
+               '-e', 'kube_network_plugin=%s' % self.options['network_plugin'], '-u',  '%s' % self.options['ansible_user'], 
+               '-e', 'ansible_ssh_user=%s' % self.options['ansible_user'], '-b', '--become-user=root', '-i', self.inventorycfg, 
+               os.path.join(self.options['kubespray_path'], 'cluster.yml')]
+        if 'ansible_opts' in self.options.keys():
+            cmd = cmd + self.options['ansible_opts'].split(' ')
+        display.display(' '.join(cmd), color='bright blue')
+        if self.options['interactive']:
+            query_yes_no('Run kubernetes cluster deployment with the above command ?')
+        display.banner('RUN PLAYBOOK')
+        self.logger.info('Running kubernetes deployment with the command: %s' % cmd)
+        self.run_command(cmd)
+        display.display('Kubernetes deployed successfuly', color='green')
+        os.kill(int(os.environ.get('SSH_AGENT_PID')), signal.SIGTERM)
