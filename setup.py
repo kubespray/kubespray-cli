76005c614b810238e3f91edc21ac2ea693844421
Smana <smainklh@gmail.com>
2016-04-03 10:56:39 +0200

    changing version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='kargo',
-    version='0.1.6',
+    version='0.1.7',
     description="Kargo kubernetes cluster deployment",
     author="Smaine Kahlouch",
     author_email='smainklh@gmail.com',
