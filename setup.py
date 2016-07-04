3421213c665640f1d2f8da16e608cd608061ba1a
Smana <smainklh@gmail.com>
2016-07-04 21:40:07 +0200

    quickfix for prepare argument

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 
 setup(
     name='kargo',
-    version='0.4.0',
+    version='0.4.1',
     description="Kargo kubernetes cluster deployment",
     author="Smaine Kahlouch",
     author_email='smainklh@gmail.com',
