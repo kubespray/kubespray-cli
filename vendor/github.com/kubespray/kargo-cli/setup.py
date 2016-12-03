b218907bcb17c4a795df0ceb0ca1ba0d8fbe37dc
Syed <syed1.mushtaq@gmail.com>
2016-12-03 17:38:37 -0500

    Update import paths and add vendor directory

---
+++ vendor/github.com/kubespray/kargo-cli/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+requirements = [
+    'cffi>=1.6.0',
+    'setuptools>=11.3',
+    'cryptography>=1.3.2',
+    'requests>=2.4.3',
+    'netaddr>=0.7.18',
+    'markupsafe>=0.23',
+    'pyasn1>=0.1.8',
+    'boto>=2.40.0',
+    'apache-libcloud>=0.20.1'
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='kargo',
+    version='0.4.6',
+    description="Kargo kubernetes cluster deployment",
+    author="Smaine Kahlouch",
+    author_email='smainklh@gmail.com',
+    url='https://github.com/kargo/kargo',
+    data_files=[
+        ('/etc/kargo', ['src/kargo/files/kargo.yml'])
+    ],
+    packages=find_packages('src'),
+    scripts=[
+        'bin/kargo'
+    ],
+    package_dir={'': 'src'},
+    package_data={'kargo': ['files/*.yml'], },
+    install_requires=requirements,
+    license="GPLv3",
+    zip_safe=False,
+    keywords='kargo',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
