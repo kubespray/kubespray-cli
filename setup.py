db433e556c6f37caeffaadca0a01a4cad9026b52
Smana <smainklh@gmail.com>
2017-06-28 17:57:31 +0200

    bump version due to renaming issues

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ my_homedir = os.path.expanduser("~")
 
 setup(
     name='kubespray',
-    version='0.4.8',
+    version='0.4.9',
     description="Kubespray kubernetes cluster deployment",
     author="Smaine Kahlouch",
     author_email='smainklh@gmail.com',
